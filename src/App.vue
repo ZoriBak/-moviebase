0618a81fbecf99a8c834b8416fcc5e367cf56903
Zori Bak <bakzori15@gmail.com>
2026-08-01 13:56:20 +0300

    Create App layout and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,95 @@
-<script setup></script>
+<script setup>
+// Import Vue's computed function
+import { computed } from 'vue'
+
+// Import the router components
+import { RouterView, useRouter } from 'vue-router'
+
+// Import our stores
+import { useAuthStore } from './stores/authStore'
+
+// PrimeVue components
+import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
+
+// Get access to the router
+const router = useRouter()
+
+// Get the authentication store
+const authStore = useAuthStore()
+
+// Navigation menu items
+const menuItems = [
+    {
+        label: 'Home',
+        icon: 'pi pi-home',
+        command: () => router.push('/')
+    },
+    {
+        label: 'Search',
+        icon: 'pi pi-search',
+        command: () => router.push('/search')
+    },
+    {
+        label: 'Favorites',
+        icon: 'pi pi-heart',
+        command: () => router.push('/favorites')
+    }
+]
+
+// Display the username only when logged in
+const username = computed(() => authStore.username)
+
+// Logout button
+function logout() {
+    authStore.logout()
+    router.push('/')
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+    <Menubar :model="menuItems">
+
+        <template #end>
+
+            <div class="menu-right">
+
+                <span
+                    v-if="authStore.isLoggedIn"
+                    class="username"
+                >
+                    {{ username }}
+                </span>
+
+                <Button
+                    v-if="authStore.isLoggedIn"
+                    label="Logout"
+                    icon="pi pi-sign-out"
+                    severity="danger"
+                    @click="logout"
+                />
+
+            </div>
+
+        </template>
+
+    </Menubar>
+
+    <RouterView />
+
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.menu-right{
+    display:flex;
+    align-items:center;
+    gap:15px;
+}
+
+.username{
+    font-weight:bold;
+}
+
+</style>
